9c6625a10b2307395716c843c1c3dcec281667ec
ConnorC04 <connorjcasey@iCloud.com>
2025-03-14 14:52:29 -0400

    breaking his stuff

--- src/test/java/io/zipcoder/PersonTest.java
+++ src/test/java/io/zipcoder/PersonTest.java
@@ -15,25 +15,25 @@ public class PersonTest {
         personcheckings = new PersonAccount(1, 1.0, "John Doe", savings);
         personsavings = new PersonAccount(1, 1.0, "John Doe", checking);
     }
-    @Test
-    public void testOverDraft(){
-
-        setUp();
-
-        boolean expectedOverDraft = true;
-        boolean expectedNotOverDraft = false;
-
-        boolean actualOverDraft = personsavings.overDraft(70.0, 80.0);
-        boolean actualNotOverDraft = personsavings.overDraft(70.0, 50.0);
-
-        boolean actualOverDraft2 = personcheckings.overDraft(70.0, 80.0);
-        boolean actualNotOverDraft2 = personcheckings.overDraft(70.0, 50.0);
-
-        Assert.assertEquals(expectedOverDraft, actualOverDraft);
-        Assert.assertEquals(expectedNotOverDraft, actualNotOverDraft);
-        Assert.assertEquals(expectedOverDraft, actualOverDraft2);
-        Assert.assertEquals(expectedNotOverDraft, actualNotOverDraft2);
-    }
+//    @Test
+//    public void testOverDraft(){
+//
+//        setUp();
+//
+//        boolean expectedOverDraft = true;
+//        boolean expectedNotOverDraft = false;
+//
+//        boolean actualOverDraft = personsavings.overDraft(70.0, 80.0);
+//        boolean actualNotOverDraft = personsavings.overDraft(70.0, 50.0);
+//
+//        boolean actualOverDraft2 = personcheckings.overDraft(70.0, 80.0);
+//        boolean actualNotOverDraft2 = personcheckings.overDraft(70.0, 50.0);
+//
+//        Assert.assertEquals(expectedOverDraft, actualOverDraft);
+//        Assert.assertEquals(expectedNotOverDraft, actualNotOverDraft);
+//        Assert.assertEquals(expectedOverDraft, actualOverDraft2);
+//        Assert.assertEquals(expectedNotOverDraft, actualNotOverDraft2);
+//    }
     @Test
     public void testInsufficientFunds(){
         setUp();
